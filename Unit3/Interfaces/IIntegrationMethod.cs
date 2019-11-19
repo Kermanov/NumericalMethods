d19f6a1d105436656fdc974322e37228dae605a0
Maksym Dovhan <kerman901@gmail.com>
2019-11-19 14:53:12 +0200

    Add rectangle method

---
+++ Unit3/Interfaces/IIntegrationMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit3.Interfaces
+{
+    public interface IIntegrationMethod
+    {
+        double Calculate(Func<double, double> function, double a, double b, int n);
+    }
+}
---
+++ Unit3/RectangleMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit3.Interfaces;
+
+namespace Unit3
+{
+    public class RectangleMethod : IIntegrationMethod
+    {
+        public double Calculate(Func<double, double> function, double a, double b, int n)
+        {
+            double h = (b - a) / n;
+            double sum = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                sum += function(a + ((2 * n + 1) / 2) * h);
+            }
+            double result = sum * h;
+
+            return result;
+        }
+    }
+}
